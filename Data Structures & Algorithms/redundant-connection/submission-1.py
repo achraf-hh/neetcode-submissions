4af7b4acdd5e63256388919221950fc8ef1234af
Achraf Badreddine <115168854+achraf-hh@users.noreply.github.com>
2026-09-10 18:45:52 +0200

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        visit = set()
+        currPath = []
+        neiMap = defaultdict(list)
+        for e, d in edges:
+            neiMap[e].append(d)
+            neiMap[d].append(e)
+        def dfs(node, prev):
+            if node in visit:
+                idx = currPath.index(node)
+                cycle = currPath[idx:]
+                cycle_edges = set()
+                for i in range(1,len(cycle)):
+                    cycle_edges.add((min(cycle[i - 1], cycle[i]), max(cycle[i - 1], cycle[i])))
+                cycle_edges.add((min(cycle[0], cycle[-1]), max(cycle[0], cycle[-1])))
+                for i in range(len(edges) - 1, 0, -1):
+                    if tuple(edges[i]) in cycle_edges:
+                        return edges[i]
+            else:
+                visit.add(node)
+                currPath.append(node)
+                for nei in neiMap[node]:
+                    if nei == prev: continue
+                    else:
+                        ans =  dfs(nei, node)
+                        if ans:
+                            return ans
+                            
+
+                currPath.pop()
+        return dfs(1, -1)
+
+
