df67d56a73b1f4d7b13e62308895163a43497fae
Achraf Badreddine <115168854+achraf-hh@users.noreply.github.com>
2026-09-25 19:52:51 +0200

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        visit = set()
+        res, cur = [], []
+
+        def dfs():
+            if len(cur) == len(nums):
+                res.append(cur.copy())
+                return
+            for i in range(len(nums)):
+                if nums[i] not in visit:
+                    visit.add(nums[i])
+                    cur.append(nums[i])
+                    dfs()
+                    visit.remove(nums[i])
+                    cur.pop()
+
+        dfs()
+        return res
